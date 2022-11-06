8843499fc2b022c1897f459e70b8dc07cf45ac8e
Nawfal Hadi <nawfalhdi@gmail.com>
2022-11-07 02:27:17 +0800

    Android Profiler

---
+++ app/src/androidTest/java/com/thatnawfal/binarsibc6challange/data/network/firebase/StorageFirebaseTest.kt
@@ -0,0 +1,19 @@
+package com.thatnawfal.binarsibc6challange.data.network.firebase
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class StorageFirebaseTest {
+
+    private lateinit var sf : StorageFirebase
+
+    @Before
+    fun setup(){
+        sf = StorageFirebase()
+    }
+
+
+}
